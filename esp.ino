44eb9e29afc8c2394df9199856dc40d0a3bcf708
Barvius <barabanshikov.oleg@yandex.ru>
2018-10-12 19:35:48 +0300

    v 0.0.1

---
+++ esp.ino
@@ -0,0 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266httpUpdate.h>
+#include <ESP8266HTTPClient.h>
+#include <Servo.h>
+
+ESP8266WebServer HTTP(80);
+
+Servo myServo;
+
+void ConnectWiFi() {
+  WiFi.begin();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void UpdateFirmware() {
+  t_httpUpdate_return ret = ESPhttpUpdate.update("192.168.1.141", 80, "/esp8266/", String(ESP.getSketchSize()));
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+
+    case HTTP_UPDATE_NO_UPDATES:
+      Serial.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+
+    case HTTP_UPDATE_OK:
+      Serial.println("HTTP_UPDATE_OK");
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  ConnectWiFi();
+  UpdateFirmware();
+  HTTP_init();
+}
+
+void loop() {
+  HTTP.handleClient();
+  delay(1);
+}
---
+++ http.ino
@@ -0,0 +1,28 @@
+void HTTP_init(void) {
+  HTTP.on("/restart", restart);
+  HTTP.on("/feed", feed_handler);
+  HTTP.begin();
+}
+
+
+void feed_handler(){
+
+  myServo.attach(14);
+  myServo.write(0);
+  delay(50);
+  HTTP.send(200, "text / plain", "OK");
+  for(int i = 0;i<6;i++){ //~10g
+    myServo.write(120);
+    delay(1000);
+    myServo.write(0);
+    delay(500);
+  }
+  myServo.detach();
+}
+
+
+void restart() {
+  HTTP.send(200, "text / plain", "Reset OK");
+  ESP.restart();
+}
+
